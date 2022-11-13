36e8813f12ab96d0f8596e3685541873524e6532
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-13 20:47:43 +0200

    add get_user_join_request

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -2,3 +2,7 @@ from aiogram import types
 from dispatcher import dp
 import config
 
+
+@dp.chat_join_request_handler()
+async def _get_user_join_request(message: types.Message) -> None:
+    print(message)
